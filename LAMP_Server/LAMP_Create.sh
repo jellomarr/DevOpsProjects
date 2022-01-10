ad336dd4d0d3ea80b6f3c20493011849b4ec08c9
Eilos <HandM@Hanss-iMac.local>
2022-01-10 22:26:00 +1100

    Added commands to create a LAMP server in any distro that is Red Hat based.;`

---
+++ LAMP_Server/LAMP_Create.sh
@@ -0,0 +1,15 @@
+
+#Install Apache
+yum install httpd -y
+systemctl start httpd.service
+systemctl enable httpd.service
+
+#Install MariaDB
+yum install mariadb mariadb-server
+mysql_secure_installation
+systemctl start mariadb 
+systemctl enable mariadb 
+
+#Install PHP
+yum install php php-mbstring  php-gd php-mysqlnd  php-zip php-soap php-ldap php-curl
+
